cccd7db95cb38d6068f85866d2408af8298ad91e
Nick Fox-Gieg <nick@fox-gieg.com>
2019-06-13 17:18:22 -0400

    start port of gvr

---
+++ Scripts/Oculus_NewController.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[RequireComponent(typeof(GvrTrackedController))]
+public class Oculus_NewController : MonoBehaviour {
+
+    public bool triggerPressed = false;
+    public bool padPressed = false;
+    public bool gripped = false;
+    public bool menuPressed = false;
+    public bool triggerDown = false;
+    public bool padDown = false;
+    public bool gripDown = false;
+    public bool menuDown = false;
+    public bool triggerUp = false;
+    public bool padUp = false;
+    public bool gripUp = false;
+    public bool menuUp = false;
+
+    public bool padDirUp = false;
+    public bool padDirDown = false;
+    public bool padDirLeft = false;
+    public bool padDirRight = false;
+    public bool padDirCenter = false;
+
+    public Vector2 touchpad = new Vector2(0f, 0f);
+
+    [HideInInspector] public Vector3 startPos = Vector3.zero;
+    [HideInInspector] public Vector3 endPos = Vector3.zero;
+    [HideInInspector] public float triggerVal;
+
+    private OVRInput.Controller ctl;
+
+    private float touchPadLimit = 0.6f; // 0.7f;
+
+    private void Awake() {
+        ctl = GetComponent<OVRInput.Controller>();
+		OVRInput.Get(OVRInput.Button.PrimaryHandTrigger, ctl);
+	}
+
+    private void Update() {
+        resetButtons();
+		//checkTriggerVal();
+		//checkPadDir();
+
+		if (OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger, ctl)) {
+            triggerPressed = true;
+            triggerDown = true;
+            startPos = transform.position;
+        } else if (OVRInput.GetUp(OVRInput.Button.PrimaryHandTrigger, ctl)) {
+            triggerPressed = false;
+            triggerUp = true;
+            endPos = transform.position;
+        }
+
+		/*
+        if (OVRInput.Get(OVRInput.Button.Down, ctl)) {
+            padPressed = true;
+            padDown = true;
+        } else if (OVRInput.Get(OVRInput.Button.Up, ctl)) {
+            padPressed = false;
+            padUp = true;
+        }
+
+        if (OVRInput.Get(OVRInput.Button.Down, ctl)) {
+            gripped = true;
+            gripDown = true;
+        } else if (OVRInput.Get(OVRInput.Button.Up, ctl)) {
+            gripped = false;
+            gripUp = true;
+        }
+
+        if (OVRInput.Get(OVRInput.Button.Down, ctl)) {
+            menuPressed = true;
+            menuDown = true;
+        } else if (OVRInput.Get(OVRInput.Button.Up, ctl)) {
+            menuPressed = false;
+            menuUp = true;
+        }
+    */
+
+	}
+
+	private void resetButtons() {
+        triggerDown = false;
+        padDown = false;
+        gripDown = false;
+        menuDown = false;
+        triggerUp = false;
+        padUp = false;
+        gripUp = false;
+        menuUp = false;
+
+        padDirUp = false;
+        padDirDown = false;
+        padDirLeft = false;
+        padDirRight = false;
+        padDirCenter = true;
+    }
+
+    //private void checkTriggerVal() {
+        //triggerVal = device.GetAxis(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger).x;
+    //}
+
+    /*
+    private void checkPadDir() {
+        touchpad = ctl.ControllerInputDevice.TouchPos; //device.GetAxis(Valve.VR.EVRButtonId.k_EButton_Axis0);
+
+        if (touchpad.y > touchPadLimit) {
+            padDirUp = true;
+            padDirDown = false;
+            padDirCenter = false;
+        } else if (touchpad.y < -touchPadLimit) {
+            padDirUp = false;
+            padDirDown = true;
+            padDirCenter = false;
+        }
+
+        if (touchpad.x > touchPadLimit) {
+            padDirLeft = true;
+            padDirRight = false;
+            padDirCenter = false;
+        } else if (touchpad.x < -touchPadLimit) {
+            padDirLeft = false;
+            padDirRight = true;
+            padDirCenter = false;
+        }
+    }
+	*/
+
+    /*
+    float defaultVibrationVal = 2f;
+
+    public void vibrateController() {
+        int ms = (int)defaultVibrationVal * 1000;
+        device.TriggerHapticPulse((ushort)ms, Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad);
+    }
+
+    public void vibrateController(float val) {
+        int ms = (int)val * 1000;
+        device.TriggerHapticPulse((ushort)ms, Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad);
+    }
+    */
+
+}
---
+++ Scripts/latkInput_Oculus.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class latkInput_Oculus : MonoBehaviour {
+
+    public Oculus_NewController ctl0;
+    public Oculus_NewController ctl1;
+    public LightningArtist latk;
+    public Renderer collisionGuideRen;
+
+    private float collisionDelay = 0.2f;
+    private float repeatDelay = 0.5f;
+
+    private void Awake() {
+        if (latk == null) latk = GetComponent<LightningArtist>();
+    }
+
+    private void Update() {
+        // draw
+        if ((ctl0.triggerPressed && !ctl0.menuPressed)) {// || Input.GetKeyDown(KeyCode.Space)) {
+            latk.clicked = true;
+        } else {
+            latk.clicked = false;
+        }
+
+        if (ctl0.triggerPressed && ctl0.menuPressed) {
+            latk.inputErase();
+        } else if (!ctl0.triggerPressed && ctl0.menuPressed) {
+            latk.inputPush();
+            latk.inputColorPick();
+        }
+
+        // new frame
+        if (ctl1.triggerDown && ctl0.menuPressed) {
+            latk.inputNewFrameAndCopy();
+            Debug.Log("Ctl: New Frame Copy");
+        } else if (ctl1.triggerDown && !ctl0.menuPressed) {
+            latk.inputNewFrame();
+            Debug.Log("Ctl: New Frame");
+        }
+
+        // show / hide all frames
+        if ((!ctl0.menuPressed && ctl1.menuDown)) {
+            latk.showOnionSkin = !latk.showOnionSkin;
+            if (latk.showOnionSkin) {
+                latk.inputShowFrames();
+            } else {
+                latk.inputHideFrames();
+            }
+        }
+
+        // ~ ~ ~ ~ ~ ~ ~ ~ ~
+
+        if (ctl0.menuPressed && ctl1.menuDown) {
+            latk.inputDeleteFrame();
+        }
+
+        // dir pad main
+        if (ctl0.padDown) {
+            if (ctl0.padDirCenter) {
+                if (latk.brushMode == LightningArtist.BrushMode.ADD) {
+                    latk.brushMode = LightningArtist.BrushMode.SURFACE;
+                } else {
+                    latk.brushMode = LightningArtist.BrushMode.ADD;
+                }
+            } else if (ctl0.padDirUp) {
+                StartCoroutine(delayedUseCollisions());
+            }
+        }
+
+        if (ctl0.padPressed) {
+            if (ctl0.padDirLeft) {
+                latk.brushSizeInc();
+            } else if (ctl0.padDirRight) {
+                latk.brushSizeDec();
+            }
+        }
+
+        // dir pad alt
+        if (ctl1.padDown) {
+            if (ctl0.menuPressed) {
+                if (ctl1.padDirCenter) {
+                    // TODO capture
+                } else if (ctl1.padDirUp) {
+                    latk.inputNewLayer();
+                } else if (ctl1.padDirLeft) {
+                    latk.inputNextLayer();
+                } else if (ctl1.padDirRight) {
+                    latk.inputPreviousLayer();
+                }
+            } else {
+                if (ctl1.padDirCenter) {
+                    latk.inputPlay();
+                } else if (ctl1.padDirUp) {
+                    latk.inputFirstFrame();
+                } else if (ctl1.padDirRight) {
+                    latk.inputFrameBack();
+                    StartCoroutine(repeatFrameBack());
+                } else if (ctl1.padDirLeft) {
+                    latk.inputFrameForward();
+                    StartCoroutine(repeatFrameForward());
+                }
+            }
+        }
+    }
+
+    IEnumerator repeatFrameForward() {
+        yield return new WaitForSeconds(repeatDelay);
+        while (ctl1.padPressed && ctl1.padDirLeft) {
+            latk.inputFrameForward();
+            yield return new WaitForSeconds(latk.frameInterval);
+        }
+    }
+
+    IEnumerator repeatFrameBack() {
+        yield return new WaitForSeconds(repeatDelay);
+        while (ctl1.padPressed && ctl1.padDirRight) {
+            latk.inputFrameBack();
+            yield return new WaitForSeconds(latk.frameInterval);
+        }
+    }
+
+    IEnumerator delayedUseCollisions() {
+        yield return new WaitForSeconds(collisionDelay);
+        if (ctl0.padDirUp) {
+            latk.useCollisions = !latk.useCollisions;
+            if (collisionGuideRen != null) collisionGuideRen.enabled = latk.useCollisions;
+        }
+    }
+
+}
